b88b19ce15ebbddff048f2b521cf370b0b487262
jHoon99 <eldkf1597@naver.com>
2025-07-12 18:11:19 +0900

    프로젝트 생성

---
+++ EscapeVision/EscapeVision/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  EscapeVision
+//
+//  Created by 조재훈 on 7/12/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ EscapeVision/EscapeVision/EscapeVisionApp.swift
@@ -0,0 +1,33 @@
+//
+//  EscapeVisionApp.swift
+//  EscapeVision
+//
+//  Created by 조재훈 on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EscapeVisionApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
