53f0b455aa6fe9d1d50d0776970ac462fef37109
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-07-24 05:59:54 +0900

    chore: 색상 Extension추가

---
+++ EscapeVision/EscapeVision/Resource/Color/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  EscapeVision
+//
+//  Created by PenguinLand on 7/23/25.
+//
+
+import SwiftUI
+extension Color {
+  // MARK: Monitor Color
+  static let green00 = Color(hex: "5FA252")
+
+}
