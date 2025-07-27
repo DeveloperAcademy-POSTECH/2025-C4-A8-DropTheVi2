30c82a9b2f99bac21bd84c290e875d4139217f05
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-07-28 07:35:18 +0900

    feat: 화면 전환에 필요한 상태 및 함수 구현

--- EscapeVision/EscapeVision/Source/Model/AppModel.swift
+++ EscapeVision/EscapeVision/Source/Model/AppModel.swift
@@ -11,11 +11,61 @@ import SwiftUI
 @MainActor
 @Observable
 class AppModel {
-    let immersiveSpaceID = "ImmersiveSpace"
-    enum ImmersiveSpaceState {
-        case closed
-        case inTransition
-        case open
-    }
-    var immersiveSpaceState = ImmersiveSpaceState.closed
+  let immersiveSpaceID = "ImmersiveSpace"
+  
+  var isWhiteoutActive = false
+  
+  enum ImmersiveSpaceState {
+    case closed
+    case inTransition
+    case open
+  }
+  
+  enum AppState {
+    case splash
+    case guideline
+    case menu
+    case playing
+  }
+  
+  var immersiveSpaceState = ImmersiveSpaceState.closed
+  var appState = AppState.splash // 앱 시작 시 가이드라인부터
+  
+  var needsImmersiveSpace: Bool {
+    return appState == .guideline || appState == .playing
+  }
+  
+  // MARK: - Game State Management
+  func showGuideline() {
+    print("가이드라인 화면 동작")
+    appState = .guideline
+  }
+  
+  func showMainMenu() {
+    print("메뉴화면으로 이동")
+    appState = .menu
+  }
+  
+  func startGame() {
+    print("🎮 게임 시작")
+    appState = .playing
+  }
+  
+  func resetToMain() {
+    print("🎮 메인으로 돌아가기")
+    appState = .menu
+  }
+  
+  // MARK: - Computed Properties
+  var isShowingGuideline: Bool {
+    return appState == .guideline
+  }
+  
+  var isGameActive: Bool {
+    return appState == .playing
+  }
+  
+  var isShowingSplash: Bool {
+    return appState == .splash
+  }
 }
