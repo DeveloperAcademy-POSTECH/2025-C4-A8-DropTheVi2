3837c71eaefc11a8864cc26eea05287a59d5b680
Penguin <149217055+Potalzz@users.noreply.github.com>
2025-07-28 07:38:16 +0900

    feat: 앱 상태에 따라 window화면 실행 및 immersive상태 변경 기능 구현

--- EscapeVision/EscapeVision/Source/App/ContentView.swift
+++ EscapeVision/EscapeVision/Source/App/ContentView.swift
@@ -5,40 +5,49 @@
 //  Created by 조재훈 on 7/12/25.
 //
 
+import Foundation
 import SwiftUI
 import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-  @State private var showARTutorial: Bool = false
+  @Environment(AppModel.self) private var appModel
+  @Environment(\.openImmersiveSpace) private var openImmersiveSpace
+  @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
   
   var body: some View {
-    VStack {
-      
-      Text("Hello, world!")
-        
-      HStack {
-        Button("튜토리얼") {
-          showARTutorial = true
+    ZStack {
+      if appModel.appState == .splash {
+        SplashView()
+      } else if appModel.appState == .guideline {
+        GuidelineWindowView()
+      } else if appModel.appState == .menu {
+        MainThemeView()
+      } else if appModel.appState == .playing {
+        VStack {
+          Text("게임 진행 중..")
         }
-        .buttonStyle(.borderedProminent)
-        .opacity(0.5)
-        
-        ToggleImmersiveSpaceButton()
-          .buttonStyle(.borderedProminent)
-          .opacity(0.5)
       }
-      .padding(.top, 50)
     }
-    .padding()
-    .fullScreenCover(isPresented: $showARTutorial, content: {
-      ARTutorial(isPresented: $showARTutorial)
-    })
-    .animation(.easeInOut(duration: 0.3), value: showARTutorial)
+    .onChange(of: appModel.needsImmersiveSpace) { oldValue, newValue in
+      Task { @MainActor in
+        if newValue && appModel.immersiveSpaceState == .closed {
+          appModel.immersiveSpaceState = .inTransition
+          
+          await openImmersiveSpace(id: appModel.immersiveSpaceID)
+        } else if !newValue && appModel.immersiveSpaceState == .open {
+          appModel.immersiveSpaceState = .inTransition
+          
+          await dismissImmersiveSpace()
+        }
+      }
+    }
   }
 }
 
-#Preview(windowStyle: .automatic) {
-  ContentView()
-    .environment(AppModel())
+#Preview() {
+  let appModel = AppModel()
+  appModel.appState = .splash
+  return ContentView()
+    .environment(appModel)
 }
